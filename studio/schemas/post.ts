f84b0c169a907969e92b61fc07f026e389ae1948
Frank Stallone <frank@designzen.co>
2023-04-24 10:41:49 -0400

    feat: ✨ Adding blockContent with inline images

--- studio/schemas/post.ts
+++ studio/schemas/post.ts
@@ -2,7 +2,7 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'post',
-  title: 'Post',
+  title: 'Blog post',
   type: 'document',
   fields: [
     defineField({
