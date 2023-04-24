23170add040ed11328bf45e8dfa7285fe7687b62
Frank Stallone <frank@designzen.co>
2023-04-24 10:52:29 -0400

    feat: ✨ Adding author and category to schema, and cleaning up image in blockContent

--- studio/schemas/blockContent.ts
+++ studio/schemas/blockContent.ts
@@ -54,17 +54,15 @@ export default defineType({
               },
             ],
           },
-          {
-            title: 'Inline image',
-            name: 'image',
-            type: 'image',
-            icon: ImageIcon,
-            options: {
-              hotspot: true,
-            },
-          },
         ],
       },
+    }),
+    // You can add additional types here. Note that you can't use
+    // primitive types such as 'string' and 'number' in the same array
+    // as a block type.
+    defineArrayMember({
+      type: 'image',
+      options: {hotspot: true},
     }),
   ],
 })
