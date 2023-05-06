a9c9ca021ef10111984d529b23fef09223f4fb62
Frank Stallone <frank@designzen.co>
2023-05-06 18:43:45 -0400

    bug: 🐛 Fixing links

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,7 +1,9 @@
 import author from './author'
 import blockContent from './blockContent'
 import category from './category'
+import gallery from './gallery'
 import post from './post'
+import product from './product'
 import siteSettings from './siteSettings'
 
-export const schemaTypes = [blockContent, post, author, category, siteSettings]
+export const schemaTypes = [blockContent, post, author, category, siteSettings, product, gallery]
