430e7067949d5323dad4c70fb74bf1b31020a5de
Frank Stallone <frank@designzen.co>
2023-05-06 18:43:51 -0400

    bug: 🐛 Fixing links

---
+++ studio/schemas/gallery.ts
@@ -0,0 +1,69 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'gallery',
+  type: 'object',
+  title: 'Gallery',
+  fields: [
+    defineField({
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [
+        {
+          name: 'image',
+          type: 'image',
+          title: 'Image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+            },
+          ],
+        },
+      ],
+      options: {
+        layout: 'grid',
+      },
+    }),
+    defineField({
+      name: 'display',
+      type: 'string',
+      title: 'Display as',
+      description: 'How should we display these images?',
+      options: {
+        list: [
+          {title: 'Stacked on top of eachother', value: 'stacked'},
+          {title: 'In-line', value: 'inline'},
+          {title: 'Carousel', value: 'carousel'},
+        ],
+        layout: 'radio', // <-- defaults to 'dropdown'
+      },
+    }),
+    defineField({
+      name: 'zoom',
+      type: 'boolean',
+      title: 'Zoom enabled',
+      description: 'Should we enable zooming of images?',
+    }),
+  ],
+  preview: {
+    select: {
+      images: 'images',
+      image: 'images.0',
+    },
+    prepare(selection) {
+      const {images, image} = selection
+
+      return {
+        title: `Gallery block of ${Object.keys(images).length} images`,
+        subtitle: `Alt text: ${image.alt}`,
+        media: image,
+      }
+    },
+  },
+})
---
+++ studio/schemas/product.ts
@@ -0,0 +1,65 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'product',
+      title: 'Product',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        defineField({
+          name: 'alt',
+          type: 'string',
+        }),
+      ],
+    }),
+    defineField({
+      name: 'gallery',
+      title: 'Gallery',
+      type: 'gallery',
+    }),
+    defineField({
+      name: 'featured',
+      title: 'Featured',
+      type: 'boolean',
+    }),
+    defineField({
+      name: 'excerpt',
+      title: 'Excerpt',
+      type: 'text',
+      rows: 4,
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'product',
+      media: 'mainImage',
+    },
+  },
+})
