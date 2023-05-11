24f6314de382aab684cedf0f68f4a20b11e86d38
Frank Stallone <frank@designzen.co>
2023-05-11 15:35:30 -0700

    feat: ✨ Adding robots.txt and sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
-import sanity from 'astro-sanity'
+import sanity from 'astro-sanity';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [sanity({
-        projectId: 'yiwm54j7',
-        dataset: 'production',
-        apiVersion: '2023-04-24',
-        useCdn: true,
-    })]
+  integrations: [
+    sanity({
+      projectId: 'yiwm54j7',
+      dataset: 'production',
+      apiVersion: '2023-04-24',
+      useCdn: true,
+    }),
+    sitemap(),
+  ],
+  site: 'https://loumarcsigns.com',
 });
