ecd1d4243ded990f0895996bf46695c7d95c04b1
Frank Stallone <frank@designzen.co>
2023-07-05 11:56:16 -0400

    feat: ✨ Adding astro/image

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,18 +1,16 @@
 import { defineConfig } from 'astro/config';
 import sanity from 'astro-sanity';
-
 import sitemap from '@astrojs/sitemap';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: 'yiwm54j7',
-      dataset: 'production',
-      apiVersion: '2023-04-24',
-      useCdn: false,
-    }),
-    sitemap(),
-  ],
-  site: 'https://loumarcsigns.com',
+  integrations: [sanity({
+    projectId: 'yiwm54j7',
+    dataset: 'production',
+    apiVersion: '2023-04-24',
+    useCdn: false
+  }), sitemap(), image()],
+  site: 'https://loumarcsigns.com'
 });
