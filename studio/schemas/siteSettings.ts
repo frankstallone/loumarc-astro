588495fac1dafd4778c1e6933c6def0678a5b98f
Frank Stallone <frank@designzen.co>
2023-07-25 13:01:07 -0400

    wip: 🚧 Setting up foundations for schema.org and small fixes

--- studio/schemas/siteSettings.ts
+++ studio/schemas/siteSettings.ts
@@ -6,24 +6,80 @@ export default defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'title',
-      title: 'Title',
+      name: 'companyName',
+      title: 'Company name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'phoneNumber',
+      title: 'Company telephone number',
       type: 'string',
     }),
     defineField({
       name: 'description',
-      title: 'Description',
+      title: 'Company description',
+      description: 'Description to be used as a default meta description',
       type: 'text',
     }),
     defineField({
       name: 'url',
-      title: 'URL',
+      title: 'Domain URL',
       type: 'url',
     }),
     defineField({
       name: 'ogImage',
       title: 'Open Graph Image',
       type: 'image',
+    }),
+    defineField({
+      name: 'schemaDotOrg',
+      title: 'Schema.org',
+      description: 'Site wide Schema.org properties',
+      type: 'object',
+      fields: [
+        {
+          name: 'streetAddress',
+          title: 'Postal Address',
+          description: '@type: PostalAddress, streetAddress',
+          type: 'string',
+        },
+        {
+          name: 'addressLocality',
+          title: 'Address Locality',
+          description: 'addressLocality',
+          type: 'string',
+        },
+        {
+          name: 'addressRegion',
+          title: 'Address Region',
+          description: 'addressRegion',
+          type: 'string',
+        },
+        {
+          name: 'postalCode',
+          title: 'Postal Code',
+          description: 'postalCode',
+          type: 'string',
+        },
+        {
+          name: 'areaServed',
+          title: 'Area Served, @type: State',
+          description: 'areaServed',
+          type: 'string',
+        },
+        {
+          name: 'areaServedSameAs',
+          title: 'Area Served, Same As',
+          description: 'sameAs',
+          type: 'string',
+        },
+        {
+          name: 'schemaDescription',
+          title: 'Description',
+          description: 'Schema.org description',
+          type: 'text',
+        },
+      ],
     }),
   ],
 })
