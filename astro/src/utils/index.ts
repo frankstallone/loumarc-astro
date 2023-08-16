db804cfd894d09061f7a62cabc9a5832d7ee522b
Frank Stallone <frank@designzen.co>
2023-08-16 17:15:16 -0400

    refactor: ♻️  Sanity images now have better image name handling

--- astro/src/utils/index.ts
+++ astro/src/utils/index.ts
@@ -6,6 +6,17 @@ interface ImageUrlBuilderOptions {
   height: number;
   format: 'webp' | 'jpg';
   quality: number;
+  index?: number;
+  imgObj: any; // Sanity image object
+}
+
+/**
+ * Turns a page title into dash-seperated lowercase string
+ * @param title Page title string
+ * @returns Dash-seperated lowercase string
+ */
+export function spaceToDash(title: string) {
+  return title.toLowerCase().replace(/\s/g, '-');
 }
 
 /**
@@ -15,12 +26,10 @@ interface ImageUrlBuilderOptions {
  * @returns SEO friendly string for image name
  */
 
-export function getImageName(
-  title: string,
+export function getSeoStrings(
+  index: number,
   secondaryInfo?: Array<string> | null
 ) {
-  // Converting page title to dashed lowercase string
-  const titleToDash = title.toLowerCase().replace(/\s/g, '-');
   // Array of SEO locations for product pages
   const seoLocations = [
     'new-jersey',
@@ -31,30 +40,29 @@ export function getImageName(
   ];
   // Array of secondary info for creating image names, if none provided, use SEO locations are default
   const secondaryInfoStrings = secondaryInfo ? secondaryInfo : seoLocations;
-  // Return page title only if secondary info is null
-  if (secondaryInfo === null) return `/${titleToDash}`;
-  // Return page title and randomly selected option from given array
-  return `/${titleToDash}-${
-    secondaryInfoStrings[
-      Math.floor(Math.random() * secondaryInfoStrings.length)
-    ]
-  }`;
+
+  // If we have an index, let's use it to return a specific string from the array
+  if (secondaryInfoStrings[index]) {
+    return `${secondaryInfoStrings[index]}`;
+  } else {
+    return `${secondaryInfoStrings[secondaryInfoStrings.length - 1]}-${
+      index - secondaryInfoStrings.length + 1
+    }`;
+  }
 }
 
 /**
  * Function that returns a vanity URL for an image
- * @param image Sanity Image Object
  * @param options ImageUrlBuilderOptions
  * @param secondaryInfo Array of strings to be used in image name
  * @returns Vanity URL string
  */
 export function getVanityURL(
-  image,
   options: ImageUrlBuilderOptions,
   secondaryInfo?: Array<string> | null
 ) {
   // Constructed URL for webp image
-  const originalURL = urlForImage(image)
+  const originalURL = urlForImage(options.imgObj)
     .width(options.width)
     .height(options.height)
     .format(options.format)
@@ -62,21 +70,37 @@ export function getVanityURL(
     .url();
   // Index of the query string
   const originalURLQueryIndex = originalURL.indexOf('?');
-  // New URL with vanity string
-  const vanityURL =
-    originalURL.slice(0, originalURLQueryIndex) +
-    getImageName(options.title, secondaryInfo) +
-    `.${options.format}` +
-    originalURL.slice(originalURLQueryIndex);
+  const pageTitleDashed = spaceToDash(options.title);
 
-  return vanityURL;
+  // Return URL with page title only if secondary info is null
+  if (secondaryInfo === null || options.index === undefined)
+    return (
+      originalURL.slice(0, originalURLQueryIndex) +
+      `/${pageTitleDashed}.${options.format}` +
+      originalURL.slice(originalURLQueryIndex)
+    );
+
+  const seoStrings = getSeoStrings(options.index, secondaryInfo);
+  // Return URL with SEO friendly image name
+  return (
+    originalURL.slice(0, originalURLQueryIndex) +
+    `/${pageTitleDashed}-${seoStrings}.${options.format}` +
+    originalURL.slice(originalURLQueryIndex)
+  );
 }
+
+/**
+ * Function that creates a Schema.org JSON-LD script for a product
+ * @param product props.productSchema in <Head> component
+ * @param siteSettings Site settings in Sanity
+ * @returns
+ */
 
 export function getProductSchema(product, siteSettings) {
   // Creating product image for schema.org
   const schemaImage = getVanityURL(
-    product.image,
     {
+      imgObj: product.image,
       title: `New Jersey ${product.title}`,
       width: 600,
       height: 450,
@@ -85,7 +109,7 @@ export function getProductSchema(product, siteSettings) {
     },
     null
   );
-
+  // Creates an array of Offers for schema.org
   function createOffer(items: Array<string>) {
     return items.map((item) => {
       return {
