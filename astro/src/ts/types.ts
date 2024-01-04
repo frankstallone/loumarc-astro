40b48ae75f2db23b0481e5511da275e020c11677
Frank Stallone <frank@designzen.co>
2024-01-04 17:57:03 -0500

    feat: ✨ Adding type safety

--- astro/src/ts/types.ts
+++ astro/src/ts/types.ts
@@ -1,3 +1,13 @@
 export type Slug =
   | 'default'
   | 'how-to-gain-more-peoples-attention-with-signs-near-your-business';
+
+export type SanityProps = {
+  title: string;
+  body: any;
+  slug: any;
+  metaDescription: string;
+  productSchema: any;
+  gallery?: any;
+  mainImage?: string;
+};
