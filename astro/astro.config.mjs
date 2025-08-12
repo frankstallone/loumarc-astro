5cb864b7a44b25d01db96f912c7ed15762ca06f1
Frank Stallone <frank@stallone.dev>
2025-08-12 09:59:16 -0400

    config: adding netlify dev features  [skip netlify]

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sanityIntegration from "@sanity/astro";
+import sanityIntegration from '@sanity/astro';
 import sitemap from '@astrojs/sitemap';
 import purgecss from 'astro-purgecss';
 import partytown from '@astrojs/partytown';
@@ -9,22 +9,27 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-      sanityIntegration({
-          projectId: 'yiwm54j7',
-          dataset: 'production',
-          apiVersion: '2023-04-24',
-          useCdn: false,
-      }),
-      sitemap(),
-      purgecss(),
-      partytown({
-          // Adds dataLayer.push as a forwarding-event.
-          config: {
-              forward: ['dataLayer.push'],
-          },
-      }),
-	],
+    sanityIntegration({
+      projectId: 'yiwm54j7',
+      dataset: 'production',
+      apiVersion: '2023-04-24',
+      useCdn: false,
+    }),
+    sitemap(),
+    purgecss(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 
   site: 'https://loumarcsigns.com',
-  adapter: netlify(),
+  adapter: netlify({
+    devFeatures: {
+      environmentVariables: true,
+      images: false,
+    },
+  }),
 });
