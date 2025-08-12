9ced07ee6bffa2a3e91bce0191a22430c1e47d65
Frank Stallone <frank@stallone.dev>
2025-08-12 09:43:03 -0400

    config: Adding Netlify adapter [skip netlify]

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,23 +4,27 @@ import sitemap from '@astrojs/sitemap';
 import purgecss from 'astro-purgecss';
 import partytown from '@astrojs/partytown';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		sanityIntegration({
-			projectId: 'yiwm54j7',
-			dataset: 'production',
-			apiVersion: '2023-04-24',
-			useCdn: false,
-		}),
-		sitemap(),
-		purgecss(),
-		partytown({
-			// Adds dataLayer.push as a forwarding-event.
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
+  integrations: [
+      sanityIntegration({
+          projectId: 'yiwm54j7',
+          dataset: 'production',
+          apiVersion: '2023-04-24',
+          useCdn: false,
+      }),
+      sitemap(),
+      purgecss(),
+      partytown({
+          // Adds dataLayer.push as a forwarding-event.
+          config: {
+              forward: ['dataLayer.push'],
+          },
+      }),
 	],
-	site: 'https://loumarcsigns.com',
+
+  site: 'https://loumarcsigns.com',
+  adapter: netlify(),
 });
