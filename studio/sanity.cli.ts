5228f7755587e6a795b3e7c7fb8cb834ca0527c0
Frank Stallone <frank@stallone.dev>
2025-12-19 05:27:30 -0500

    Upgrade Sanity Studio to v5 (#33)

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,5 +5,8 @@ export default defineCliConfig({
     projectId: 'yiwm54j7',
     dataset: 'production',
   },
-  studioHost: 'loumarc'
+  studioHost: 'loumarc',
+  deployment: {
+    appId: 'c029eaf97b3eb774c36d6d3f',
+  },
 })
